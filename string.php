70e9599363895ab5441633037ca8b4245e82d843
Iqbalhossen07 <iqbalhossen0711@gmail.com>
2024-05-18 22:49:11 +0600

    assignment 2 complete

---
+++ string.php
@@ -0,0 +1,10 @@
+<?php
+
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+foreach ($strings as $string) {
+    $vowelCount = preg_match_all('/[aeiou]/i', $string, $matches);
+    $reversedString = strrev($string);
+    echo "Original String: $string, Vowel Count: $vowelCount, Reversed String: $reversedString\n";
+}
+?>
